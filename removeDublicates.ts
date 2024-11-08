e1adc46e56882e702bf62de8878ae9c0ee907173
Chowdhury Imam Hossain <121708728+ImamHossain22@users.noreply.github.com>
2024-11-08 19:37:57 +0600

    no code here

--- removeDublicates.ts
+++ removeDublicates.ts
@@ -1,7 +1,1 @@
-let array = [1, 2, 2, 3, 4, 3, 5, 4, 6, 5, 45, 45, 23, 65, 65];
 
-const removeArrayElement = (array: number[]): number[] => {
-    return array.filter((item,
-        index) => array.indexOf(item) === index);
-}
-console.log(removeArrayElement(array));
